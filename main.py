9821035e6e7b8f4e75367bdcafe2105ab5d3b62c
caglakarakus <karakuscaglanur@gmail.com>
2025-03-01 19:49:39 +0300

    after tests failed,function fixed all test are done and passed

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 def int_to_roman(num):
+
     """
     Convert an integer to a Roman numeral.
 
     :param num: Integer value between 1 and 3999 inclusive.
     :return: A string representing the Roman numeral of the integer.
     """
+
+    roman_dict = {
+        1:"I", 4:"IV" , 5:"V", 9:"IX" , 10:"X", 40:"XL", 50:"L",90:"XC",
+        100:"C",400:"CD", 500:"D",
+        900:"CM", 1000:"M"
+    }
+
+    result=""
+    for value in sorted(roman_dict.keys(),reverse=True):
+        while num>= value:
+            result+=roman_dict[value]
+            num=num-value
+
+
+    return result
+
